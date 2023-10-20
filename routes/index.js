2f765403c78ce64cbf9ebc3cdbfb22d36bff1b87
hfzhart <zmykalo.denys@student.uzhnu.edu.ua>
2023-10-20 14:29:01 +0300

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressHello' });
+  res.render('index', { title: 'ExpressHelloWorld' });
 });
 
 module.exports = router;
